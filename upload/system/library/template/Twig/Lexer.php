a1e75bee179b580f72d8861f452382d15eb5d50e
condor2 <condor20_05@yahoo.it>
2020-05-21 13:58:35 +0300

    two php7.4 bugs fixed

--- upload/system/library/template/Twig/Lexer.php
+++ upload/system/library/template/Twig/Lexer.php
@@ -160,7 +160,7 @@ class Twig_Lexer implements Twig_LexerInterface
 
         // push the template text first
         $text = $textContent = substr($this->code, $this->cursor, $position[1] - $this->cursor);
-        if (isset($this->positions[2][$this->position][0])) {
+        if ($this->options['whitespace_trim'] === $this->positions[2][$this->position][0]) {
             $text = rtrim($text);
         }
         $this->pushToken(Twig_Token::TEXT_TYPE, $text);
