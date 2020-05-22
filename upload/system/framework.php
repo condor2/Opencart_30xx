245611480d0f1ba3f0e0519b93b6511d7218dd26
condor2 <condor20_05@yahoo.it>
2020-05-22 18:04:58 +0300

    minicart not updating fix
    https://forum.opencart.com/viewtopic.php?t=206449#p786660

--- upload/system/framework.php
+++ upload/system/framework.php
@@ -72,6 +72,9 @@ $registry->set('request', new Request());
 // Response
 $response = new Response();
 $response->addHeader('Content-Type: text/html; charset=utf-8');
+$response->addHeader('Expires: Thu, 19 Nov 1981 08:52:00 GMT');
+$response->addHeader('Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0');
+$response->addHeader('Pragma: no-cache');
 $response->setCompression($config->get('config_compression'));
 $registry->set('response', $response);
 
