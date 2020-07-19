9a9041097edf560331664f66b7968eeeed8505e5
condor2 <condor20_05@yahoo.it>
2020-07-19 19:15:01 +0300

    changed session db engine
    https://github.com/opencart/opencart/pull/7115/files

--- upload/system/library/session/db.php
+++ upload/system/library/session/db.php
@@ -1,65 +1,59 @@
 <?php
-/*
-CREATE TABLE IF NOT EXISTS `session` (
-  `session_id` varchar(32) NOT NULL,
-  `data` text NOT NULL,
-  `expire` datetime NOT NULL,
-  PRIMARY KEY (`session_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-*/
+
 namespace Session;
+
 final class DB {
-	public $expire = 3600;
-	
+	public $maxlifetime;
+
 	public function __construct($registry) {
 		$this->db = $registry->get('db');
-		
-		$this->expire = ini_get('session.gc_maxlifetime');
 
-		$this->db->query("DELETE FROM `" . DB_PREFIX . "session` WHERE expire < DATE_SUB(NOW(), INTERVAL " . ((int)$this->expire + 86400) . " SECOND)");
+		$this->maxlifetime = ini_get('session.gc_maxlifetime') !== null ? (int)ini_get('session.gc_maxlifetime') : 1440;
+
+		$this->gc();
 	}
-	
+
 	public function read($session_id) {
-		$query = $this->db->query("SELECT `data` FROM `" . DB_PREFIX . "session` WHERE session_id = '" . $this->db->escape($session_id) . "' AND expire > '" . $this->db->escape(date('Y-m-d H:i:s'))  . "'");
-		
+		$query = $this->db->query("SELECT `data` FROM `" . DB_PREFIX . "session` WHERE `session_id` = '" . $this->db->escape($session_id) . "' AND `expire` > '" . $this->db->escape(date('Y-m-d H:i:s', time())) . "'");
+
 		if ($query->num_rows) {
 			return json_decode($query->row['data'], true);
 		} else {
 			return false;
 		}
 	}
-	
+
 	public function write($session_id, $data) {
 		if ($session_id) {
-			$this->db->query("REPLACE INTO `" . DB_PREFIX . "session` SET session_id = '" . $this->db->escape($session_id) . "', `data` = '" . $this->db->escape($data ? json_encode($data) : '') . "', expire = '" . $this->db->escape(date('Y-m-d H:i:s', time() + $this->expire)) . "'");
+			$this->db->query("REPLACE INTO `" . DB_PREFIX . "session` SET `session_id` = '" . $this->db->escape($session_id) . "', `data` = '" . $this->db->escape(json_encode($data)) . "', `expire` = '" . $this->db->escape(date('Y-m-d H:i:s', time() + (int)$this->maxlifetime)) . "'");
 		}
-		
+
 		return true;
 	}
-	
+
 	public function destroy($session_id) {
-		$this->db->query("DELETE FROM `" . DB_PREFIX . "session` WHERE session_id = '" . $this->db->escape($session_id) . "'");
-		
+		$this->db->query("DELETE FROM `" . DB_PREFIX . "session` WHERE `session_id` = '" . $this->db->escape($session_id) . "'");
+
 		return true;
 	}
-	
-	public function gc($expire) {
-		if (ini_get('session.gc_divisor')) {
-			$gc_divisor = ini_get('session.gc_divisor');
+
+	public function gc() {
+		if (ini_get('session.gc_divisor') && $gc_divisor = (int)ini_get('session.gc_divisor')) {
+			$gc_divisor = $gc_divisor === 0 ? 100 : $gc_divisor;
 		} else {
-			$gc_divisor = 1;
+			$gc_divisor = 100;
 		}
 
 		if (ini_get('session.gc_probability')) {
-			$gc_probability = ini_get('session.gc_probability');
+			$gc_probability = (int)ini_get('session.gc_probability');
 		} else {
 			$gc_probability = 1;
 		}
 
-		if ((rand() % $gc_divisor) < $gc_probability) {
-			$this->db->query("DELETE FROM `" . DB_PREFIX . "session` WHERE expire < '" . $this->db->escape(date('Y-m-d H:i:s', time())) . "'");
-		}
+		if (mt_rand() / mt_getrandmax() > $gc_probability / $gc_divisor) {
+			$this->db->query("DELETE FROM `" . DB_PREFIX . "session` WHERE `expire` < '" . $this->db->escape(date('Y-m-d H:i:s', time())) . "'");
 
-		return true;
+			return true;
+		}
 	}
 }
