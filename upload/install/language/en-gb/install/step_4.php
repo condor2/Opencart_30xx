49c866278433d9ba2d3f1509cd210bbecb178d6a
condor2 <condor20_05@yahoo.it>
2020-07-30 11:29:43 +0300

    Step_4 language updated

--- upload/install/language/en-gb/install/step_4.php
+++ upload/install/language/en-gb/install/step_4.php
@@ -6,11 +6,9 @@ $_['heading_title']               = 'Installation complete';
 $_['text_step_4']                 = 'Ready to start selling!';
 $_['text_catalog']                = 'Go to your Online Shop';
 $_['text_admin']                  = 'Login to your Administration';
-$_['text_loading']                = 'Loading modules...';
 $_['text_extension']              = 'visit the extensions store';
 $_['text_mail']                   = 'Join the mailing list';
 $_['text_mail_description']       = 'Stay informed of OpenCart updates and events.';
-$_['text_amazon_pay']             = 'Amazon Pay is a trusted payment solution which allows customers to check in and out using information already stored in their Amazon account. It\'s fast, simple and secure....<a href="https://pay.amazon.com/uk/signup" target="_blank">More information</a>';
 $_['text_facebook']               = 'Like us on Facebook';
 $_['text_facebook_description']   = 'Tell us how much you like OpenCart!';
 $_['text_facebook_visit']         = 'Visit our Facebook page';
@@ -20,14 +18,9 @@ $_['text_forum_visit']            = 'Visit our forums';
 $_['text_commercial']             = 'Commercial support';
 $_['text_commercial_description'] = 'Development services from OpenCart partners';
 $_['text_commercial_visit']       = 'Visit our partner page';
-$_['text_price']                  = 'Price';
-$_['text_view']                   = 'View details';
-$_['text_download']               = 'Download';
-$_['text_downloads']              = 'Downloads';
 
 // Button
 $_['button_mail']                 = 'Join here';
-$_['button_setup']                = 'Set-up now';
 
 // Error
 $_['error_warning']               = 'Don\'t forget to delete your installation directory!';
