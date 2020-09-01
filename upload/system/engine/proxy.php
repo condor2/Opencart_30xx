efac1aab04022d9e03fbd0a7a5b78f6fb1dbd8e1
condor2 <condor20_05@yahoo.it>
2020-09-01 15:24:44 +0300

    Master Branch Engine updated

--- upload/system/engine/proxy.php
+++ upload/system/engine/proxy.php
@@ -11,44 +11,35 @@
 * Proxy class
 */
 class Proxy {
-    /**
-     * 
-     *
-     * @param	string	$key
-     */	
-	public function __get($key) {
+	/**
+	 *
+	 *
+	 * @param	string	$key
+	 */
+	public function &__get($key) {
 		return $this->{$key};
-	}	
-
-    /**
-     * 
-     *
-     * @param	string	$key
-	 * @param	string	$value
-     */	
-	public function __set($key, $value) {
-		 $this->{$key} = $value;
 	}
-	
-	public function __call($key, $args) {
-		$arg_data = array();
-		
-		$args = func_get_args();
-		
-		foreach ($args as $arg) {
-			if ($arg instanceof Ref) {
-				$arg_data[] =& $arg->getRef();
-			} else {
-				$arg_data[] =& $arg;
-			}
-		}
-		
-		if (isset($this->{$key})) {		
-			return call_user_func_array($this->{$key}, $arg_data);	
+
+	/**
+	 *
+	 *
+	 * @param	string	$key
+	 * @param	string	$value
+	 */
+	public function __set($key, $value) {
+		$this->{$key} = $value;
+	}
+
+	public function __call($method, $args) {
+		// Hack for pass-by-reference
+		foreach ($args as $key => &$value);
+
+		if (isset($this->{$method})) {
+			return call_user_func_array($this->{$method}, $args);
 		} else {
 			$trace = debug_backtrace();
-			
-			exit('<b>Notice</b>:  Undefined property: Proxy::' . $key . ' in <b>' . $trace[1]['file'] . '</b> on line <b>' . $trace[1]['line'] . '</b>');
+
+			exit('<b>Notice</b>:  Undefined property: Proxy::' . $key . ' in <b>' . $trace[0]['file'] . '</b> on line <b>' . $trace[0]['line'] . '</b>');
 		}
 	}
 }
