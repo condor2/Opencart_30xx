e519e37dda620e5c98dc39fe580c881fbc3b2583
condor2 <condor20_05@yahoo.it>
2021-02-02 11:10:27 +0200

    Added width for td image

--- upload/admin/controller/report/online.php
+++ upload/admin/controller/report/online.php
@@ -26,7 +26,7 @@ class ControllerReportOnline extends Controller {
 		$url = '';
 
 		if (isset($this->request->get['filter_customer'])) {
-			$url .= '&filter_customer=' . urlencode(html_entity_decode($this->request->get['filter_customer'], ENT_QUOTES, 'UTF-8'));
+			$url .= '&filter_customer=' . urlencode(html_entity_decode((string)$this->request->get['filter_customer'], ENT_QUOTES, 'UTF-8'));
 		}
 
 		if (isset($this->request->get['filter_ip'])) {
