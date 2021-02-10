41e67551d4855e687eb45f5711b9d15658f1e155
condor2 <condor20_05@yahoo.it>
2021-02-10 11:05:55 +0200

    Added missed code

--- upload/catalog/controller/product/product.php
+++ upload/catalog/controller/product/product.php
@@ -234,6 +234,10 @@ class ControllerProductProduct extends Controller {
 
 			$data['tab_review'] = sprintf($this->language->get('tab_review'), $product_info['reviews']);
 
+			$data['error_upload_size'] = sprintf($this->language->get('error_upload_size'), $this->config->get('config_file_max_size'));
+
+			$data['config_file_max_size'] = $this->config->get('config_file_max_size');
+
 			$data['product_id'] = (int)$this->request->get['product_id'];
 			$data['manufacturer'] = $product_info['manufacturer'];
 			$data['manufacturers'] = $this->url->link('product/manufacturer/info', 'manufacturer_id=' . $product_info['manufacturer_id']);
