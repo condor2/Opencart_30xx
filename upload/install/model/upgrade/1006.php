93d0439e69c6aa8f67cc79c6a84b6d7291990ee6
condor2 <condor20_05@yahoo.it>
2021-08-09 10:18:22 +0300

    More space work

--- upload/install/model/upgrade/1006.php
+++ upload/install/model/upgrade/1006.php
@@ -187,7 +187,7 @@ class ModelUpgrade1006 extends Model {
 
 	    // Open the source directory to read in files
 	    $i = new DirectoryIterator($src);
-	    foreach($i as $f) {
+	    foreach ($i as $f) {
 	        if($f->isFile() && !file_exists("$dest/" . $f->getFilename())) {
 	            @rename($f->getRealPath(), "$dest/" . $f->getFilename());
 	        } elseif(!$f->isDot() && $f->isDir()) {
