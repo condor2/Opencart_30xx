6e39e29c411a979f3c2ee076f14ba1858c3da304
condor2 <condor20_05@yahoo.it>
2021-08-30 09:28:29 +0300

    Display only enabled countries on Address
    https://github.com/opencart/opencart/pull/10169/files

--- upload/catalog/model/account/address.php
+++ upload/catalog/model/account/address.php
@@ -34,7 +34,7 @@ class ModelAccountAddress extends Model {
 		$address_query = $this->db->query("SELECT DISTINCT * FROM `" . DB_PREFIX . "address` WHERE `address_id` = '" . (int)$address_id . "' AND `customer_id` = '" . (int)$this->customer->getId() . "'");
 
 		if ($address_query->num_rows) {
-			$country_query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "country` WHERE `country_id` = '" . (int)$address_query->row['country_id'] . "'");
+			$country_query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "country` WHERE `country_id` = '" . (int)$address_query->row['country_id'] . "' AND `status` = '1'");
 
 			if ($country_query->num_rows) {
 				$country = $country_query->row['name'];
@@ -90,7 +90,7 @@ class ModelAccountAddress extends Model {
 		$query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "address` WHERE `customer_id` = '" . (int)$this->customer->getId() . "'");
 
 		foreach ($query->rows as $result) {
-			$country_query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "country` WHERE `country_id` = '" . (int)$result['country_id'] . "'");
+			$country_query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "country` WHERE `country_id` = '" . (int)$result['country_id'] . "' AND `status` = '1'");
 
 			if ($country_query->num_rows) {
 				$country = $country_query->row['name'];
