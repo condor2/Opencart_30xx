4712fe16ba7ff74179b7ae793d7d6315fbee174e
condor2 <condor20_05@yahoo.it>
2021-10-20 14:55:40 +0300

    Removed duplicated language variable - security.php

--- upload/admin/language/en-gb/common/security.php
+++ upload/admin/language/en-gb/common/security.php
@@ -4,7 +4,6 @@ $_['heading_title']    = 'Important Security Notification!';
 
 // Text
 $_['text_success']     = 'Success: You have modified the storage folder!';
-$_['text_admin']       = 'Edit admin/config.php and change';
 $_['text_security']    = 'It is very important that you move the storage directory outside of the web directory (e.g. public_html, www or htdocs).';
 $_['text_choose']      = 'Choose how to move the storage directory';
 $_['text_automatic']   = 'Automatically Move';
