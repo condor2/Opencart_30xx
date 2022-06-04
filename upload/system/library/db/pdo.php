b8715be81cc50411b48079c990e7d42b48d75863
condor2 <condor20_05@yahoo.it>
2022-06-04 08:18:35 +0300

    PDO updated like in 4.0
    https://github.com/opencart/opencart/blob/master/upload/system/library/db/pdo.php

--- upload/system/library/db/pdo.php
+++ upload/system/library/db/pdo.php
@@ -5,7 +5,11 @@ class PDO {
 	private $data = array();
 	private $affected;
 
-	public function __construct($hostname, $username, $password, $database, $port = '3306') {
+	public function __construct($hostname, $username, $password, $database, $port = '') {
+		if (!$port) {
+			$port = '3306';
+		}
+
 		try {
 			$pdo = @new \PDO('mysql:host=' . $hostname . ';port=' . $port . ';dbname=' . $database, $username, $password, [\PDO::ATTR_PERSISTENT => false]);
 		} catch (\PDOException $e) {
@@ -19,7 +23,9 @@ class PDO {
 	}
 
 	public function query($sql) {
-		$statement = $this->connection->prepare(preg_replace('/(?:\'\:)([a-z0-9]*.)(?:\')/', ':$1', $sql));
+		$sql = preg_replace('/(?:\'\:)([a-z0-9]*.)(?:\')/', ':$1', $sql);
+
+		$statement = $this->connection->prepare($sql);
 
 		try {
 			if ($statement && $statement->execute($this->data)) {
@@ -29,7 +35,7 @@ class PDO {
 					$data = $statement->fetchAll(\PDO::FETCH_ASSOC);
 
 					$result = new \stdClass();
-					$result->row = (isset($data[0]) ? $data[0] : array());
+					$result->row = isset($data[0]) ? $data[0] : array();
 					$result->rows = $data;
 					$result->num_rows = count($data);
 					$this->affected = 0;
@@ -37,13 +43,19 @@ class PDO {
 					return $result;
 				} else {
 					$this->affected = $statement->rowCount();
+
+					return true;
 				}
 
 				$statement->closeCursor();
+			} else {
+				return true;
 			}
 		} catch (\PDOException $e) {
 			throw new \Exception('Error: ' . $e->getMessage() . ' Error Code : ' . $e->getCode() . ' <br />' . $sql);
 		}
+
+		return false;
 	}
 
 	public function escape($value) {
@@ -77,6 +89,6 @@ class PDO {
 	 *
 	 */
 	public function __destruct() {
-		$this->connection = '';
+		unset($this->connection);
 	}
 }
