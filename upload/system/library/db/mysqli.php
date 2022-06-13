bb917c93bda4dc954d18c41ad5c42703c38770e8
condor2 <condor20_05@yahoo.it>
2022-06-13 11:45:49 +0300

    MySQLi updated
    https://github.com/opencart/opencart/pull/10437/files

--- upload/system/library/db/mysqli.php
+++ upload/system/library/db/mysqli.php
@@ -3,27 +3,27 @@ namespace DB;
 class MySQLi {
 	private $connection;
 
-	public function __construct($hostname, $username, $password, $database, $port = '3306') {
-		try {
-			$mysqli = @new \MySQLi($hostname, $username, $password, $database, $port);
-		} catch (\mysqli_sql_exception $e) {
-			throw new \Exception('Error: Could not make a database link using ' . $username . '@' . $hostname . '!');
+	public function __construct($hostname, $username, $password, $database, $port = '') {
+		if (!$port) {
+			$port = '3306';
 		}
 
-		if (!$mysqli->connect_errno) {
+		try {
+			$mysqli = @new \MySQLi($hostname, $username, $password, $database, $port);
+
 			$this->connection = $mysqli;
 			$this->connection->report_mode = MYSQLI_REPORT_ERROR;
 			$this->connection->set_charset('utf8');
 			$this->connection->query("SET SESSION sql_mode = 'NO_ZERO_IN_DATE,NO_ENGINE_SUBSTITUTION'");
-		} else {
+		} catch (\mysqli_sql_exception $e) {
 			throw new \Exception('Error: Could not make a database link using ' . $username . '@' . $hostname . '!');
 		}
 	}
 
 	public function query($sql) {
-		$query = $this->connection->query($sql);
+		try {
+			$query = $this->connection->query($sql);
 
-		if (!$this->connection->errno) {
 			if ($query instanceof \mysqli_result) {
 				$data = array();
 
@@ -44,7 +44,7 @@ class MySQLi {
 			} else {
 				return true;
 			}
-		} else {
+		} catch (\mysqli_sql_exception $e) {
 			throw new \Exception('Error: ' . $this->connection->error  . '<br />Error No: ' . $this->connection->errno . '<br />' . $sql);
 		}
 	}
@@ -82,7 +82,7 @@ class MySQLi {
 		if ($this->connection) {
 			$this->connection->close();
 
-			$this->connection = '';
+			unset($this->connection);
 		}
 	}
 }
