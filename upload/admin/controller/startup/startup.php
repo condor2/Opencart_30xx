b665c4d5e8b0ffaa8df7c19083a536e5079419aa
condor2 <condor20_05@yahoo.it>
2022-12-21 15:04:12 +0200

    Removed session code from admin startup controller

--- upload/admin/controller/startup/startup.php
+++ upload/admin/controller/startup/startup.php
@@ -20,41 +20,6 @@ class ControllerStartupStartup extends Controller {
 			$this->db->query("SET time_zone = '" . $this->db->escape(date('P')) . "'");
 		}
 
-		// Session
-		$session = new Session($this->config->get('session_engine'), $this->registry);
-		$this->registry->set('session', $session);
-
-		if (isset($this->request->cookie[$this->config->get('session_name')])) {
-			$session_id = $this->request->cookie[$this->config->get('session_name')];
-		} else {
-			$session_id = '';
-		}
-
-		$session->start($session_id);
-
-		// Setting the cookie path to the store front so admin users can login to cutomers accounts.
-		$path = dirname($_SERVER['PHP_SELF']);
-
-		$path = substr($path, 0, strrpos($path, '/')) . '/';
-
-		// Update the session lifetime
-		if ($this->config->get('config_session_expire')) {
-			$this->config->set('session_expire', $this->config->get('config_session_expire'));
-		}
-
-		$expire = (int)$this->config->get('config_session_expire');
-
-		// Require higher security for session cookies
-		$option = array(
-			'expires'  => $this->config->get('config_session_expire') ? time() + (int)$this->config->get('config_session_expire') : 0,
-			'path'     => !empty($_SERVER['PHP_SELF']) ? $path : '',
-			'secure'   => $this->request->server['HTTPS'],
-			'httponly' => false,
-			'SameSite' => $this->config->get('config_session_samesite')
-		);
-
-		setcookie($this->config->get('session_name'), $session->getId(), $option);
-
 		// Response output compression level
 		if ($this->config->get('config_compression')) {
 			$this->response->setCompression($this->config->get('config_compression'));
