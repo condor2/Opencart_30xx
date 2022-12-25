b2bcf2ce92e291be74f52299181ad68ba299cc13
condor2 <condor20_05@yahoo.it>
2022-12-25 13:20:26 +0200

    Order admin alert design same as custommer order alert

--- upload/catalog/controller/mail/order.php
+++ upload/catalog/controller/mail/order.php
@@ -377,31 +377,39 @@ class ControllerMailOrder extends Controller {
 		$order_info = $this->model_checkout_order->getOrder($order_id);
 		
 		if ($order_info && !$order_info['order_status_id'] && $order_status_id && in_array('order', (array)$this->config->get('config_mail_alert'))) {	
-			$this->load->language('mail/order_alert');
-			
+
+			// Load the language for any mails that might be required to be sent out
+			$language = new Language($order_info['language_code']);
+			$language->load($order_info['language_code']);
+			$language->load('mail/order_alert');
+
 			// HTML Mail
-			$data['text_received'] = $this->language->get('text_received');
-			$data['text_order_id'] = $this->language->get('text_order_id');
-			$data['text_date_added'] = $this->language->get('text_date_added');
-			$data['text_order_status'] = $this->language->get('text_order_status');
-			$data['text_product'] = $this->language->get('text_product');
-			$data['text_total'] = $this->language->get('text_total');
-			$data['text_comment'] = $this->language->get('text_comment');
-			
-			$data['order_id'] = $order_info['order_id'];
-			$data['date_added'] = date($this->language->get('date_format_short'), strtotime($order_info['date_added']));
-
-			$order_status_query = $this->db->query("SELECT * FROM " . DB_PREFIX . "order_status WHERE order_status_id = '" . (int)$order_status_id . "' AND language_id = '" . (int)$this->config->get('config_language_id') . "'");
-
-			if ($order_status_query->num_rows) {
-				$data['order_status'] = $order_status_query->row['name'];
-			} else {
-				$data['order_status'] = '';
-			}
+			$data['text_greeting'] = sprintf($language->get('text_greeting'), $order_info['store_name']);
+			$data['text_link'] = $language->get('text_link');
+			$data['text_download'] = $language->get('text_download');
+			$data['text_order_detail'] = $language->get('text_order_detail');
+			$data['text_instruction'] = $language->get('text_instruction');
+			$data['text_order_id'] = $language->get('text_order_id');
+			$data['text_date_added'] = $language->get('text_date_added');
+			$data['text_payment_method'] = $language->get('text_payment_method');
+			$data['text_shipping_method'] = $language->get('text_shipping_method');
+			$data['text_email'] = $language->get('text_email');
+			$data['text_telephone'] = $language->get('text_telephone');
+			$data['text_ip'] = $language->get('text_ip');
+			$data['text_order_status'] = $language->get('text_order_status');
+			$data['text_payment_address'] = $language->get('text_payment_address');
+			$data['text_shipping_address'] = $language->get('text_shipping_address');
+			$data['text_product'] = $language->get('text_product');
+			$data['text_model'] = $language->get('text_model');
+			$data['text_quantity'] = $language->get('text_quantity');
+			$data['text_price'] = $language->get('text_price');
+			$data['text_total'] = $language->get('text_total');
+			$data['text_footer'] = $language->get('text_footer');
+			$data['text_comment'] = $language->get('text_comment');
 
 			$data['store_url'] = $this->config->get('config_url');
 			$data['store'] = html_entity_decode($this->config->get('config_name'), ENT_QUOTES, 'UTF-8');
-			
+
 			$this->load->model('tool/image');
 
 			if (is_file(DIR_IMAGE . html_entity_decode($this->config->get('config_logo'), ENT_QUOTES, 'UTF-8'))) {
@@ -410,8 +418,101 @@ class ControllerMailOrder extends Controller {
 				$data['logo'] = '';
 			}
 
+			$data['customer_id'] = $order_info['customer_id'];
+
+			$data['order_id'] = $order_info['order_id'];
+			$data['date_added'] = date($language->get('date_format_short'), strtotime($order_info['date_added']));
+			$data['payment_method'] = $order_info['payment_method'];
+			$data['shipping_method'] = $order_info['shipping_method'];
+			$data['email'] = $order_info['email'];
+			$data['telephone'] = $order_info['telephone'];
+			$data['ip'] = $order_info['ip'];
+
+			$order_status_query = $this->db->query("SELECT * FROM " . DB_PREFIX . "order_status WHERE order_status_id = '" . (int)$order_status_id . "' AND language_id = '" . (int)$order_info['language_id'] . "'");
+	
+			if ($order_status_query->num_rows) {
+				$data['order_status'] = $order_status_query->row['name'];
+			} else {
+				$data['order_status'] = '';
+			}
+
+			if ($comment && $notify) {
+				$data['comment'] = nl2br($comment);
+			} else {
+				$data['comment'] = '';
+			}
+
+			if ($order_info['payment_address_format']) {
+				$format = $order_info['payment_address_format'];
+			} else {
+				$format = '{firstname} {lastname}' . "\n" . '{company}' . "\n" . '{address_1}' . "\n" . '{address_2}' . "\n" . '{city} {postcode}' . "\n" . '{zone}' . "\n" . '{country}';
+			}
+
+			$find = array(
+				'{firstname}',
+				'{lastname}',
+				'{company}',
+				'{address_1}',
+				'{address_2}',
+				'{city}',
+				'{postcode}',
+				'{zone}',
+				'{zone_code}',
+				'{country}'
+			);
+
+			$replace = array(
+				'firstname' => $order_info['payment_firstname'],
+				'lastname'  => $order_info['payment_lastname'],
+				'company'   => $order_info['payment_company'],
+				'address_1' => $order_info['payment_address_1'],
+				'address_2' => $order_info['payment_address_2'],
+				'city'      => $order_info['payment_city'],
+				'postcode'  => $order_info['payment_postcode'],
+				'zone'      => $order_info['payment_zone'],
+				'zone_code' => $order_info['payment_zone_code'],
+				'country'   => $order_info['payment_country']
+			);
+
+			$data['payment_address'] = str_replace(["\r\n", "\r", "\n"], '<br />', preg_replace(["/\s\s+/", "/\r\r+/", "/\n\n+/"], '<br />', trim(str_replace($find, $replace, $format))));
+
+			if ($order_info['shipping_address_format']) {
+				$format = $order_info['shipping_address_format'];
+			} else {
+				$format = '{firstname} {lastname}' . "\n" . '{company}' . "\n" . '{address_1}' . "\n" . '{address_2}' . "\n" . '{city} {postcode}' . "\n" . '{zone}' . "\n" . '{country}';
+			}
+
+			$find = array(
+				'{firstname}',
+				'{lastname}',
+				'{company}',
+				'{address_1}',
+				'{address_2}',
+				'{city}',
+				'{postcode}',
+				'{zone}',
+				'{zone_code}',
+				'{country}'
+			);
+
+			$replace = array(
+				'firstname' => $order_info['shipping_firstname'],
+				'lastname'  => $order_info['shipping_lastname'],
+				'company'   => $order_info['shipping_company'],
+				'address_1' => $order_info['shipping_address_1'],
+				'address_2' => $order_info['shipping_address_2'],
+				'city'      => $order_info['shipping_city'],
+				'postcode'  => $order_info['shipping_postcode'],
+				'zone'      => $order_info['shipping_zone'],
+				'zone_code' => $order_info['shipping_zone_code'],
+				'country'   => $order_info['shipping_country']
+			);
+
+			$data['shipping_address'] = str_replace(["\r\n", "\r", "\n"], '<br />', preg_replace(["/\s\s+/", "/\r\r+/", "/\n\n+/"], '<br />', trim(str_replace($find, $replace, $format))));
+
 			$this->load->model('tool/upload');
-			
+
+			// Products
 			$data['products'] = array();
 
 			$order_products = $this->model_checkout_order->getOrderProducts($order_id);
@@ -445,10 +546,12 @@ class ControllerMailOrder extends Controller {
 					'model'    => $order_product['model'],
 					'quantity' => $order_product['quantity'],
 					'option'   => $option_data,
+					'price'    => $this->currency->format($order_product['price'] + ($this->config->get('config_tax') ? $order_product['tax'] : 0), $order_info['currency_code'], $order_info['currency_value']),
 					'total'    => html_entity_decode($this->currency->format($order_product['total'] + ($this->config->get('config_tax') ? ($order_product['tax'] * $order_product['quantity']) : 0), $order_info['currency_code'], $order_info['currency_value']), ENT_NOQUOTES, 'UTF-8')
 				);
 			}
-			
+
+			// Vouchers
 			$data['vouchers'] = array();
 			
 			$order_vouchers = $this->model_checkout_order->getOrderVouchers($order_id);
@@ -484,7 +587,7 @@ class ControllerMailOrder extends Controller {
 			$mail->setTo($this->config->get('config_order_email'));
 			$mail->setFrom($this->config->get('config_email'));
 			$mail->setSender(html_entity_decode($order_info['store_name'], ENT_QUOTES, 'UTF-8'));
-			$mail->setSubject(html_entity_decode(sprintf($this->language->get('text_subject'), $this->config->get('config_name'), $order_info['order_id']), ENT_QUOTES, 'UTF-8'));
+			$mail->setSubject(html_entity_decode(sprintf($language->get('text_subject'), $this->config->get('config_name'), $order_info['order_id']), ENT_QUOTES, 'UTF-8'));
 			$mail->setHtml($this->load->view('mail/order_alert', $data));
 			$mail->send();
 
--- upload/catalog/language/en-gb/mail/order_alert.php
+++ upload/catalog/language/en-gb/mail/order_alert.php
@@ -1,10 +1,23 @@
 <?php
 // Text
-$_['text_subject']      = '%s - Order %s';
-$_['text_received']     = 'You have received an order.';
-$_['text_order_id']     = 'Order ID:';
-$_['text_date_added']   = 'Date Added:';
-$_['text_order_status'] = 'Order Status:';
-$_['text_product']      = 'Products';
-$_['text_total']        = 'Totals';
-$_['text_comment']      = 'The comments for your order are:';
+$_['text_subject']          = '%s - Order %s';
+$_['text_greeting']         = 'You have received an order.';
+$_['text_order_detail']     = 'Order Details';
+$_['text_order_id']         = 'Order ID:';
+$_['text_date_added']       = 'Date Added:';
+$_['text_order_status']     = 'Order Status:';
+$_['text_payment_method']   = 'Payment Method:';
+$_['text_shipping_method']  = 'Shipping Method:';
+$_['text_email']            = 'E-mail:';
+$_['text_telephone']        = 'Telephone:';
+$_['text_ip']               = 'IP Address:';
+$_['text_payment_address']  = 'Payment Address';
+$_['text_shipping_address'] = 'Shipping Address';
+$_['text_products']         = 'Products';
+$_['text_product']          = 'Product';
+$_['text_model']            = 'Model';
+$_['text_quantity']         = 'Quantity';
+$_['text_price']            = 'Price';
+$_['text_order_total']      = 'Order Totals';
+$_['text_total']            = 'Total';
+$_['text_comment']          = 'The comments for your order are:';
