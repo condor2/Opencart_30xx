1a57967eaa24424f5ed523751b81ff0baf823813
condor2 <condor20_05@yahoo.it>
2023-04-15 18:39:40 +0000

    Update mail.php

--- upload/system/library/mail.php
+++ upload/system/library/mail.php
@@ -11,14 +11,15 @@
 * Mail class
 */
 class Mail {
-	protected $to = '';
-	protected $from = '';
-	protected $sender = '';
-	protected $reply_to = '';
-	protected $subject = '';
-	protected $text = '';
-	protected $html = '';
+	protected $to;
+	protected $from;
+	protected $sender;
+	protected $reply_to;
+	protected $subject;
+	protected $text;
+	protected $html;
 	protected $attachments = array();
+	public $parameter;
 
 	/**
 	 * Constructor
@@ -28,88 +29,89 @@ class Mail {
  	*/
 	public function __construct($adaptor = 'mail') {
 		$class = 'Mail\\' . $adaptor;
-
+		
 		if (class_exists($class)) {
-			$this->adaptor = $class;
+			$this->adaptor = new $class();
 		} else {
-			throw new \Exception('Error: Could not load mail adaptor ' . $adaptor . '!');
-		}
+			trigger_error('Error: Could not load mail adaptor ' . $adaptor . '!');
+			exit();
+		}	
 	}
-
+	
 	/**
-     *
+     * 
      *
      * @param	mixed	$to
      */
 	public function setTo($to) {
 		$this->to = $to;
 	}
-
+	
 	/**
-     *
+     * 
      *
      * @param	string	$from
      */
 	public function setFrom($from) {
 		$this->from = $from;
 	}
-
+	
 	/**
-     *
+     * 
      *
      * @param	string	$sender
      */
 	public function setSender($sender) {
 		$this->sender = $sender;
 	}
-
+	
 	/**
-     *
+     * 
      *
      * @param	string	$reply_to
      */
 	public function setReplyTo($reply_to) {
 		$this->reply_to = $reply_to;
 	}
-
+	
 	/**
-     *
+     * 
      *
      * @param	string	$subject
      */
 	public function setSubject($subject) {
 		$this->subject = $subject;
 	}
-
+	
 	/**
-     *
+     * 
      *
      * @param	string	$text
      */
 	public function setText($text) {
 		$this->text = $text;
 	}
-
+	
 	/**
-     *
+     * 
      *
      * @param	string	$html
      */
 	public function setHtml($html) {
 		$this->html = $html;
 	}
-
+	
 	/**
-     *
+     * 
      *
      * @param	string	$filename
      */
 	public function addAttachment($filename) {
 		$this->attachments[] = $filename;
 	}
-
+	
 	/**
-     *
+     * 
      *
      */
 	public function send() {
@@ -129,16 +131,14 @@ class Mail {
 			throw new \Exception('Error: E-Mail subject required!');
 		}
 
-		if (!$this->text && !$this->html) {
+		if ((!$this->text) && (!$this->html)) {
 			throw new \Exception('Error: E-Mail message required!');
 		}
-
-		$mail_data = array();
-
-		foreach (get_object_vars($this) as $key => $value) $mail_data[$key] = $value;
-
-		$mail = new $this->adaptor($mail_data);
-
-		return $mail->send();
+		
+		foreach (get_object_vars($this) as $key => $value) {
+			$this->adaptor->$key = $value;
+		}
+		
+		$this->adaptor->send();
 	}
 }
