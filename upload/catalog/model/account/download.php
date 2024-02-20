1743351cacbea46c2eed98529987cd73656ef94f
condor2 <condor20_05@yahoo.it>
2024-02-20 15:11:32 +0200

    Typo Download controller

--- upload/catalog/model/account/download.php
+++ upload/catalog/model/account/download.php
@@ -13,9 +13,9 @@ class ModelAccountDownload extends Model {
 			$query = $this->db->query("SELECT d.`filename`, d.`mask` FROM `" . DB_PREFIX . "order` o LEFT JOIN `" . DB_PREFIX . "order_product` op ON (o.`order_id` = op.`order_id`) LEFT JOIN `" . DB_PREFIX . "product_to_download` p2d ON (op.`product_id` = p2d.`product_id`) LEFT JOIN `" . DB_PREFIX . "download` d ON (p2d.`download_id` = d.`download_id`) WHERE o.`customer_id` = '" . (int)$this->customer->getId() . "' AND (" . implode(" OR ", $implode) . ") AND d.`download_id` = '" . (int)$download_id . "'");
 
 			return $query->row;
-		} else {
-			return;
 		}
+
+		return [];
 	}
 
 	public function getDownloads($start = 0, $limit = 20) {
@@ -39,9 +39,9 @@ class ModelAccountDownload extends Model {
 			$query = $this->db->query("SELECT DISTINCT d.`download_id`, o.`order_id`, o.`date_added`, dd.`name`, d.`filename` FROM `" . DB_PREFIX . "order` o LEFT JOIN `" . DB_PREFIX . "order_product` op ON (o.`order_id` = op.`order_id`) LEFT JOIN `" . DB_PREFIX . "product_to_download` p2d ON (op.`product_id` = p2d.`product_id`) LEFT JOIN `" . DB_PREFIX . "download` d ON (p2d.`download_id` = d.`download_id`) LEFT JOIN `" . DB_PREFIX . "download_description` dd ON (d.`download_id` = dd.`download_id`) WHERE o.`customer_id` = '" . (int)$this->customer->getId() . "' AND o.`store_id` = '" . (int)$this->config->get('config_store_id') . "' AND dd.`language_id` = '" . (int)$this->config->get('config_language_id') . "' AND (" . implode(" OR ", $implode) . ") ORDER BY o.`date_added` DESC LIMIT " . (int)$start . "," . (int)$limit);
 
 			return $query->rows;
-		} else {
-			return [];
 		}
+
+		return [];
 	}
 
 	public function getTotalDownloads() {
@@ -57,9 +57,9 @@ class ModelAccountDownload extends Model {
 			$query = $this->db->query("SELECT COUNT(*) AS `total` FROM `" . DB_PREFIX . "order` o LEFT JOIN `" . DB_PREFIX . "order_product` op ON (o.`order_id` = op.`order_id`) LEFT JOIN `" . DB_PREFIX . "product_to_download` p2d ON (op.`product_id` = p2d.`product_id`) WHERE o.`customer_id` = '" . (int)$this->customer->getId() . "' AND (" . implode(" OR ", $implode) . ")");
 
 			return $query->row['total'];
-		} else {
-			return 0;
 		}
+
+		return 0;
 	}
 
 	public function addDownloadReport($download_id, $ip, $country = '') {
