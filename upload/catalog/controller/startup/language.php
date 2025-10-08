6c845fc0e670741faf6118a69e29f42be1bf72ec
condor2 <condor20_05@yahoo.it>
2025-10-08 17:19:33 +0300

    Code style fixes

--- upload/catalog/controller/startup/language.php
+++ upload/catalog/controller/startup/language.php
@@ -1,52 +1,53 @@
 <?php
 class ControllerStartupLanguage extends Controller {
-    public function index() {
+	public function index() {
 		// Language
-        $this->load->model('localisation/language');
+		$this->load->model('localisation/language');
 
-        $languages = $this->model_localisation_language->getLanguages();
+		$languages = $this->model_localisation_language->getLanguages();
 
-        $language_codes = array_column($languages, 'language_id', 'code');
+		$language_codes = array_column($languages, 'language_id', 'code');
 
-        $code = '';
+		$code = '';
 
-        if (!empty($this->request->cookie['language']) && array_key_exists($this->request->cookie['language'], $language_codes)) {
-            $code = $this->request->cookie['language'];
-        }
+		if (!empty($this->request->cookie['language']) && array_key_exists($this->request->cookie['language'], $language_codes)) {
+			$code = $this->request->cookie['language'];
+		}
 
 		// Language Detection
-        if (!$code && !empty($this->request->server['HTTP_ACCEPT_LANGUAGE'])) {
-            $browser_languages = explode(',', $this->request->server['HTTP_ACCEPT_LANGUAGE']);
-            $detect = '';
+		if (!$code && !empty($this->request->server['HTTP_ACCEPT_LANGUAGE'])) {
+			$browser_languages = explode(',', $this->request->server['HTTP_ACCEPT_LANGUAGE']);
+			$detect = '';
 
-            foreach ($browser_languages as $browser_language) {
-                $browser_language = strtolower(trim($browser_language));
+			// Try using local to detect the language
+			foreach ($browser_languages as $browser_language) {
+				$browser_language = strtolower(trim($browser_language));
 
-                foreach ($languages as $key => $value) {
-                    if ($value['status']) {
-                        $locale = array_map('strtolower', explode(',', $value['locale']));
+				foreach ($languages as $key => $value) {
+					if ($value['status']) {
+						$locale = array_map('strtolower', explode(',', $value['locale']));
 
-                        if (in_array($browser_language, $locale)) {
-                            $detect = $key;
-                            break 2;
-                        }
-                    }
-                }
-            }
+						if (in_array($browser_language, $locale)) {
+							$detect = $key;
+							break 2;
+						}
+					}
+				}
+			}
 
-            // Fallback: verificare cod limbă folder
-            if (!$detect) {
-                foreach ($browser_languages as $browser_language) {
-                    $browser_language = strtolower(trim($browser_language));
-                    if (array_key_exists($browser_language, $language_codes)) {
-                        $detect = $browser_language;
-                        break;
-                    }
-                }
-            }
+			// Try using local to detect the language
+			if (!$detect) {
+				foreach ($browser_languages as $browser_language) {
+					$browser_language = strtolower(trim($browser_language));
+					if (array_key_exists($browser_language, $language_codes)) {
+						$detect = $browser_language;
+						break;
+					}
+				}
+			}
 
-            $code = $detect ?: '';
-        }
+			$code = $detect ?: '';
+		}
 
 		if (!array_key_exists($code, $language_codes)) {
 			$code = $this->config->get('config_language');
@@ -64,12 +65,12 @@ class ControllerStartupLanguage extends Controller {
 		}
 
 		// Overwrite the default language object
-        $language = new Language($code);
-        $language->load($code);
-        $this->registry->set('language', $language);
+		$language = new Language($code);
+		$language->load($code);
+		$this->registry->set('language', $language);
 
 		// Set the config language_id
-        $this->config->set('config_language_id', $language_codes[$code]);
-        $this->config->set('config_language', $code);
-    }
+		$this->config->set('config_language_id', $language_codes[$code]);
+		$this->config->set('config_language', $code);
+	}
 }
